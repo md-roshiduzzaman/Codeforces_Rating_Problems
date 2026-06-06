d3bc5302d91d01084c1497bff91e471ae432b2fd
Md. Roshiduzzaman ( Ratul ) <roshiduzzaman1913@gmail.com>
2026-06-06 10:12:23 +0600

    Create 59_A_Word.cpp
    Implement a program to convert string case based on letter counts.

---
+++ Rating_800/59_A_Word.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+
+    int up=0, low=0;
+
+    for(char c : s)
+    {
+        if(isupper(c))
+        {
+            up++;
+        }
+        else
+        {
+            low++;
+        }
+    }
+
+    if(up > low)
+    {
+        for(char &c : s)
+        {
+            c = toupper(c);
+        }
+    }
+    else
+    {
+        for(char &c : s)
+        {
+            c = tolower(c);
+        }
+    }
+
+    cout<<s<<endl;
+
+    return 0;
+}
